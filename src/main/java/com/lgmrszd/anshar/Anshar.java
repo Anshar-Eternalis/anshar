ef14204a6b6fca2823327e36ea1d8653acb72386
eliatlarge <eliatlarge@gmail.com>
2023-12-21 21:39:49 -0600

    added cardinal components dependency

--- src/main/java/com/lgmrszd/anshar/Anshar.java
+++ src/main/java/com/lgmrszd/anshar/Anshar.java
@@ -6,10 +6,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Anshar implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("anshar");
+	
+	public static final String MOD_ID = "anshar";
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
