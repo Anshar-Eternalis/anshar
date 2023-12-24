5cd414cd55e7ae8322656312ff527c613b0748be
eliatlarge <eliatlarge@gmail.com>
2023-12-24 10:11:16 -0600

    I'm really good at programming guys trust me

--- src/main/java/com/lgmrszd/anshar/frequency/FrequencyNetwork.java
+++ src/main/java/com/lgmrszd/anshar/frequency/FrequencyNetwork.java
@@ -4,6 +4,7 @@ import java.util.Set;
 
 import net.minecraft.block.entity.BeaconBlockEntity;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.util.math.BlockPos;
 
 import java.util.UUID;
 
@@ -13,7 +14,7 @@ public class FrequencyNetwork {
     private UUID id;
     private IFrequencyIdentifier IFreqID;
     private EmbeddedStorage storage;
-    private Set<BeaconBlockEntity> beacons;
+    private Set<BlockPos> beacons;
 
     public FrequencyNetwork(UUID id) {
         this.id = id;
@@ -23,7 +24,7 @@ public class FrequencyNetwork {
         return id;
     }
 
-    public Set<BeaconBlockEntity> getBeacons(){
+    public Set<BlockPos> getBeacons(){
         return this.beacons;
     }
 
