7b038464aff543d84aadcadb7f4fc53195360884
hama <hama@hama.industries>
2023-12-24 10:18:53 -0600

    added FrequencyNetwork and friends (#1)
    * added FrequencyNetwork and friends
    
    * I'm really good at programming guys trust me

---
+++ src/main/java/com/lgmrszd/anshar/frequency/FrequencyNetwork.java
@@ -0,0 +1,43 @@
+package com.lgmrszd.anshar.frequency;
+
+import java.util.Set;
+
+import net.minecraft.block.entity.BeaconBlockEntity;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.util.math.BlockPos;
+
+import java.util.UUID;
+
+import com.lgmrszd.anshar.storage.EmbeddedStorage;
+
+public class FrequencyNetwork {
+    private UUID id;
+    private IFrequencyIdentifier IFreqID;
+    private EmbeddedStorage storage;
+    private Set<BlockPos> beacons;
+
+    public FrequencyNetwork(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public Set<BlockPos> getBeacons(){
+        return this.beacons;
+    }
+
+    public EmbeddedStorage getStorage(){
+        return this.storage;
+    }
+
+    public void readFromNbt(NbtCompound tag) {
+
+    }
+
+    public void writeToNbt(NbtCompound tag) {
+
+    }
+
+}
