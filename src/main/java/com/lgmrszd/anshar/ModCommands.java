13d5ecee0dd307113a19cd14a450e03c561d4019
Lgmrszd <lgmrszd@disroot.org>
2023-12-29 13:53:52 +0300

    Added OP command to open inventory by frequency

--- src/main/java/com/lgmrszd/anshar/ModCommands.java
+++ src/main/java/com/lgmrszd/anshar/ModCommands.java
@@ -1,8 +1,12 @@
 package com.lgmrszd.anshar;
 
 import com.lgmrszd.anshar.frequency.NetworkManagerComponent;
+import com.lgmrszd.anshar.storage.EmbeddedStorage;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.command.argument.UuidArgumentType;
+import net.minecraft.screen.GenericContainerScreenHandler;
+import net.minecraft.screen.SimpleNamedScreenHandlerFactory;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
 
@@ -39,6 +43,32 @@ public class ModCommands {
                                                 return -1;
                                             });
                                 })
+                                .then(literal("storage")
+                                        .executes(context -> {
+                                            ServerWorld world = context.getSource().getWorld();
+                                            if (world == null) return -1;
+                                            ServerPlayerEntity player = context.getSource().getPlayer();
+                                            if (player == null) return -1;
+                                            final UUID networkID = UuidArgumentType.getUuid(context, "Network ID");
+                                            return NetworkManagerComponent.KEY.get(world.getLevelProperties())
+                                                    .getNetwork(networkID)
+                                                    .map(frequencyNetwork -> {
+                                                        EmbeddedStorage inventory = frequencyNetwork.getStorage();
+                                                        player.openHandledScreen(new SimpleNamedScreenHandlerFactory((syncId, playerInv, playerx) -> {
+                                                            return GenericContainerScreenHandler.createGeneric9x3(syncId, playerInv, inventory);
+                                                        }, Text.literal("[")
+                                                                .append(Text.translatable("block.minecraft.beacon"))
+                                                                .append("] ")
+                                                                .append(Text.translatable("container.enderchest"))));
+                                                        return 0;
+                                                    })
+                                                    .orElseGet(() -> {
+                                                        context.getSource().sendFeedback(() -> Text.literal("Invalid Frequency Network with ID " + networkID.toString()),
+                                                                false);
+                                                        return -1;
+                                                    });
+                                        })
+                                )
                         )
                         .then(literal("list")
                                 .executes(context -> {
