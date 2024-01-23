54e0fc9554253eb42e3b12f98dd0ea84995d8414
eliatlarge <eliatlarge@gmail.com>
2024-01-22 21:13:35 -0600

    a little more rendering mixin cleanup

--- src/client/java/com/lgmrszd/anshar/mixin/client/render/WorldRendererMixin.java
+++ src/client/java/com/lgmrszd/anshar/mixin/client/render/WorldRendererMixin.java
@@ -1,20 +1,26 @@
 package com.lgmrszd.anshar.mixin.client.render;
 
+import org.jetbrains.annotations.Nullable;
 import org.joml.Matrix4f;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.lgmrszd.anshar.transport.PlayerTransportComponent;
 
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.render.BackgroundRenderer;
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.render.LightmapTextureManager;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
 
 @Mixin(WorldRenderer.class)
@@ -27,6 +33,11 @@ public class WorldRendererMixin {
         Entity entity = camera.getFocusedEntity();
         if (entity instanceof ClientPlayerEntity player) anshar$isInNetwork = PlayerTransportComponent.KEY.get(player).isInNetwork();
         else anshar$isInNetwork = false;
+    }
+
+    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/BackgroundRenderer;render(Lnet/minecraft/client/render/Camera;FLnet/minecraft/client/world/ClientWorld;IF)V"))
+    public void anshar$backgroundRenderOverride(Camera camera, float tickDelta, ClientWorld world, int viewDistance, float skyDarkness) {
+        if (!anshar$isInNetwork) BackgroundRenderer.render(camera, tickDelta, world, viewDistance, skyDarkness);
     }
 
     @Inject(method = "renderSky(Lnet/minecraft/client/util/math/MatrixStack;Lorg/joml/Matrix4f;FLnet/minecraft/client/render/Camera;ZLjava/lang/Runnable;)V", at = @At("HEAD"), cancellable = true)
