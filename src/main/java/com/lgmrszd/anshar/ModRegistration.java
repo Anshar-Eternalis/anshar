5c07210652db8fd97a6573b0ff2a558b7dcbde7f
Eleanor Young <developer@nausicaea.net>
2025-08-16 23:12:59 +0200

    bugfix: replace TypedActionResult with ActionResult

--- src/main/java/com/lgmrszd/anshar/ModRegistration.java
+++ src/main/java/com/lgmrszd/anshar/ModRegistration.java
@@ -23,7 +23,6 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.TypedActionResult;
 import net.neoforged.fml.config.ModConfig;
 
 public class ModRegistration {
@@ -80,13 +79,13 @@ public class ModRegistration {
             return container.onUse(player, world, hand, hitResult);
         });
         UseItemCallback.EVENT.register((player, world, hand) -> {
-            if (!player.isSneaking() || !(player instanceof ServerPlayerEntity serverPlayer)) return TypedActionResult.pass(null);
+            if (!player.isSneaking() || !(player instanceof ServerPlayerEntity serverPlayer)) return ActionResult.PASS;
             ItemStack stack = hand == Hand.MAIN_HAND ? player.getMainHandStack() : player.getOffHandStack();
-            if (!stack.isOf(Items.END_CRYSTAL)) return TypedActionResult.pass(null);
+            if (!stack.isOf(Items.END_CRYSTAL)) return ActionResult.PASS;
             EndCrystalItemContainer container = ModApi.END_CRYSTAL_ITEM.find(stack, null);
-            if (container == null || container.getBeaconPos().isEmpty()) return TypedActionResult.pass(null);
+            if (container == null || container.getBeaconPos().isEmpty()) return ActionResult.PASS;
             container.clearBeaconPos(serverPlayer);
-            return TypedActionResult.success(stack);
+            return ActionResult.SUCCESS;
         });
     }
 
